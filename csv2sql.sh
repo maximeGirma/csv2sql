70a6cf83eb91b1e6bc61c0685ecc773f380c7f02
Maxime Girma <maxime.girma@hotmail.fr>
2020-10-22 23:45:28 +0200

    table name folllow file name

--- csv2sql.sh
+++ csv2sql.sh
@@ -2,22 +2,25 @@
 # bash v4
 
 
-# TODO : parametrize (filename, output as sqlscript ...)
+# TODO : parametrize (output as sqlscript ...)
 # TODO : use var instead of hard coded var
 # TODO : Last row in table is NULL
-# TODO :  argument to open mysql cli after import
+# TODO : argument to open mysql cli after import
+# TODO : handle UTF-8 encoding
 
 docker rm -f some-mysql
 docker network rm some-mysql-network
 
 
-DB_NAME="test"
+INPUT_FILENAME=$1
+DB_NAME="data_csv"
+TABLE_NAME=${INPUT_FILENAME%.*}
 
 
-create_table_statement="CREATE TABLE IF NOT EXISTS test.test (id INT NOT NULL AUTO_INCREMENT,"
+create_table_statement="CREATE TABLE IF NOT EXISTS $DB_NAME.$TABLE_NAME (id INT NOT NULL AUTO_INCREMENT,"
 
 # get first line of csv
-keys=$(head -n 1 data.csv)
+keys=$(head -n 1 $INPUT_FILENAME)
 
 # replace commas with spaces to iterate on bash array
 keys=$(echo $keys | sed 's/,/ /g')
@@ -33,7 +36,7 @@ create_table_statement="${create_table_statement} PRIMARY KEY (id))"
 
 
 # prepare import csv statement from csv keys
-import_csv_statement="load data local infile '/data.csv' into table test.test fields terminated by ',' enclosed by '\\\"' lines terminated by '\n' IGNORE 1 LINES ("
+import_csv_statement="load data local infile '$INPUT_FILENAME' into table $DB_NAME.$TABLE_NAME fields terminated by ',' enclosed by '\\\"' lines terminated by '\n' IGNORE 1 LINES ("
 
 for key in $keys
 do
@@ -48,7 +51,7 @@ import_csv_statement="${import_csv_statement%?} );"
 docker network create some-mysql-network
 
 #create stack
-docker run -d --name some-mysql --network some-mysql-network -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 -v /home/maxime/projects/docker/db/data.csv:/data.csv mysql:latest
+docker run -d --name some-mysql --network some-mysql-network -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 -v /home/maxime/projects/docker/db/$INPUT_FILENAME:/$INPUT_FILENAME mysql:latest
 
 
 # create db
@@ -57,7 +60,7 @@ docker exec some-mysql bash -c "echo ..."
 
 sleep 8
 
-docker exec some-mysql bash -c "mysql -uroot -ppassword <<< \"create database test\""
+docker exec some-mysql bash -c "mysql -uroot -ppassword <<< \"create database $DB_NAME\""
 docker exec some-mysql bash -c "mysql -uroot -ppassword <<< \"${create_table_statement}\""
 
 echo $import_csv_statement
@@ -66,6 +69,6 @@ docker exec some-mysql bash -c "mysql -uroot -ppassword <<< \"SET GLOBAL local_i
 docker exec some-mysql bash -c "mysql -uroot -ppassword --local-infile  <<< \"${import_csv_statement}\""
 
 
-docker run -it --network some-mysql-network --rm mysql mysql -hsome-mysql -uroot -ppassword "test"
+docker run -it --network some-mysql-network --rm mysql mysql -hsome-mysql -uroot -ppassword -A "$DB_NAME"
 
 
