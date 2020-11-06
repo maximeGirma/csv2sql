e0d075b0327a5644c0b873220bfc033513644ce7
Maxime Girma <maxime.girma@hotmail.fr>
2020-11-06 11:31:51 +0100

    use password from conf to create container

--- csv2sql.sh
+++ csv2sql.sh
@@ -3,11 +3,8 @@
 
 
 # TODO : parametrize (output as sqlscript ...)
-# TODO : Last row in table is NULL
 # TODO : argument to open mysql cli after import
-# TODO : handle UTF-8 encoding
 # wait for container startup and not for time
-# fix insecure command line password
 
 # https://stackoverflow.com/questions/1101957/are-there-any-standard-exit-status-codes-in-linux
 
@@ -16,7 +13,7 @@
 #define EX_USAGE        64      /* command line usage error */
 #define EX_DATAERR      65      /* data format error */
 #define EX_NOINPUT      66      /* cannot open input */    
-#define EX_NOUSER       67      /* addressee unknown */    
+#define EX_NOUSER       67      /* addressee unknown */   
 #define EX_NOHOST       68      /* host name unknown */
 #define EX_UNAVAILABLE  69      /* service unavailable */
 #define EX_SOFTWARE     70      /* internal software error */
@@ -32,12 +29,15 @@
 MYSQL_VERSION=8.0.22
 EXTERNAL_CONTAINER_PORT=3338
 INTERNAL_CONTAINER_PORT=3306
-
+# get password from conf file & trim space
+ROOT_PASSWORD=$(awk -F "=" '/password/ {print $2}' ./conf/connection.cnf | tr -d ' ')
 
 INPUT_FILENAME=$1
 # delemiter must be between quotes "" or ''
 DELIMITER=$2
-ABSOLUTE_PATH=$INPUT_FILENAME
+CURRENT_DIR=$(pwd)
+
+ABSOLUTE_FILE_PATH=$INPUT_FILENAME
 FILENAME=${1##*/}
 DB_NAME="data_csv"
 TABLE_NAME=${FILENAME%.*}
@@ -46,7 +46,7 @@ TABLE_NAME=${FILENAME%.*}
 if test -f "$FILENAME"; then
     echo "FILE $FILE ok"
 else
-    echo "FILE $ABSOLUTE_PATH not found : Be sure to use absolute path"
+    echo "FILE $ABSOLUTE_FILE_PATH not found : Be sure to use absolute path"
     exit 74
 fi
 
@@ -58,7 +58,7 @@ docker network rm $TABLE_NAME-mysql-network
 create_table_statement="CREATE TABLE IF NOT EXISTS $DB_NAME.$TABLE_NAME (id INT NOT NULL AUTO_INCREMENT,"
 
 # get first line of csv
-keys=$(head -n 1 $ABSOLUTE_PATH)
+keys=$(head -n 1 $ABSOLUTE_FILE_PATH)
 
 # replace commas with spaces to iterate on bash array
 keys=$(echo $keys | sed "s/$DELIMITER/ /g")
@@ -71,7 +71,7 @@ do
 done
 create_table_statement="${create_table_statement} PRIMARY KEY (id))"
 
-echo $create_table_statement
+
 
 # prepare import csv statement from csv keys
 import_csv_statement="load data local infile '/$FILENAME' into table $DB_NAME.$TABLE_NAME fields terminated by '$DELIMITER' enclosed by '\\\"' lines terminated by '\n' IGNORE 1 LINES ("
@@ -89,20 +89,20 @@ import_csv_statement="${import_csv_statement%?} );"
 docker network create $TABLE_NAME-mysql-network
 
 #create stack
-docker run -d --name $TABLE_NAME-mysql --network $TABLE_NAME-mysql-network -e MYSQL_ROOT_PASSWORD=password -p $EXTERNAL_CONTAINER_PORT:$INTERNAL_CONTAINER_PORT -v $ABSOLUTE_PATH:/$FILENAME mysql:$MYSQL_VERSION --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -d --name $TABLE_NAME-mysql --network $TABLE_NAME-mysql-network -e MYSQL_ROOT_PASSWORD=$ROOT_PASSWORD -p $EXTERNAL_CONTAINER_PORT:$INTERNAL_CONTAINER_PORT -v $ABSOLUTE_FILE_PATH:/$FILENAME -v "$CURRENT_DIR/conf":/conf mysql:$MYSQL_VERSION --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # wait for mysql start up
 sleep 8
 
 # create db and table
-docker exec $TABLE_NAME-mysql bash -c "mysql -uroot -ppassword <<< \"create database $DB_NAME\""
-docker exec $TABLE_NAME-mysql bash -c "mysql -uroot -ppassword <<< \"${create_table_statement}\""
+docker exec $TABLE_NAME-mysql bash -c "mysql --defaults-extra-file=/conf/connection.cnf <<< \"create database $DB_NAME\""
+docker exec $TABLE_NAME-mysql bash -c "mysql --defaults-extra-file=/conf/connection.cnf <<< \"${create_table_statement}\""
 
 # populate table
-docker exec $TABLE_NAME-mysql bash -c "mysql -uroot -ppassword <<< \"SET GLOBAL local_infile = 1;\""
-docker exec $TABLE_NAME-mysql bash -c "mysql -uroot -ppassword --local-infile  <<< \"${import_csv_statement}\""
+docker exec $TABLE_NAME-mysql bash -c "mysql --defaults-extra-file=/conf/connection.cnf <<< \"SET GLOBAL local_infile = 1;\""
+docker exec $TABLE_NAME-mysql bash -c "mysql --defaults-extra-file=/conf/connection.cnf --local-infile  <<< \"${import_csv_statement}\""
 
 # run interactive cli
-docker run -it --network $TABLE_NAME-mysql-network --rm mysql mysql -h$TABLE_NAME-mysql -uroot -ppassword --default-character-set=utf8 -A "$DB_NAME"
+docker exec -it $TABLE_NAME-mysql mysql --defaults-extra-file=/conf/connection.cnf -h$TABLE_NAME-mysql --default-character-set=utf8 -A "$DB_NAME" 
 
 
